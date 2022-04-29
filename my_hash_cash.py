102252f529c9f77364339f3e52a9db52a7b78bf9
Your Name <your@email>
2022-04-28 20:27:51 -0400

    :)

--- my_hash_cash.py
+++ my_hash_cash.py
@@ -6,14 +6,14 @@ def verify_pow(challenge, zero_bits, proof):
 
 #challenge should be a string, zero_bits should be an integer, proof should be an integer  
 def find_pow(challenge, zero_bits):
-    proof=1;
+    proof=1
     
     while(1):
         input = challenge + str(proof)
                 
         if hashlib.sha256((input).encode('utf-8')).hexdigest().startswith('0' * int(zero_bits / 4)):
-            break;
+            break
         
         proof+=1
         
-    return proof; 
+    return proof
